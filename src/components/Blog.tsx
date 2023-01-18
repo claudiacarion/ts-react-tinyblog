6a5bc849e92fe239b830026c232a37cccfcb91c6
ClaudiaC <ClaudiaC@ClaudiaC.lan>
2023-01-18 17:41:14 +0100

    incomplete, hides all or shows all

--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -1,71 +1,79 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { IBlogProps } from '../types/types';
 import Post from './Post';
 import '../styles/blog.css';
 
-const Blog = ({ data }: IBlogProps) => (
+const Blog = ({ data }: IBlogProps) => {
+  const [section, setSection] = useState(false);
+
+  const toggleClass = () => {
+    setSection(!section);
+  };
+
+  return (
   <main className='blog-section'>
     <section className='category-section'>
-      <h3 className='category'>crime</h3>
+      <h3 onClick={ toggleClass } className='category'>crime</h3>
         {data.map(posts => {
           const { tags } = posts;
           if (tags.includes('crime')) {
-            return <>
+            return <div className={section ? 'show-crime' : 'hide'}>
               <Post posts={posts} />
-            </>;
+            </div>;
           }
           return null;
         })}
     </section>
     <section className='category-section'>
-    <h3 className='category'>english</h3>
+    <h3 onClick={ toggleClass } className='category'>english</h3>
         {data.map(posts => {
           const { tags } = posts;
           if (tags.includes('english')) {
-            return <>
+            return <div className={section ? 'show-english' : 'hide'}>
               <Post posts={posts} />
-            </>;
+            </div>;
           }
           return null;
         })}
     </section>
     <section className='category-section'>
-    <h3 className='category'>love</h3>
+    <h3 className='category' onClick={ toggleClass }>love</h3>
         {data.map(posts => {
           const { tags } = posts;
           if (tags.includes('love')) {
-            return <>
+            return <div className={section ? 'show-love' : 'hide'}>
               <Post posts={posts} />
-            </>;
+            </div>;
           }
           return null;
         })}
     </section>
     <section className='category-section'>
-    <h3 className='category'>magical</h3>
+    <h3 className='category' onClick={ toggleClass }>magical</h3>
         {data.map(posts => {
           const { tags } = posts;
           if (tags.includes('magical')) {
-            return <>
+            return <div className={section ? 'show-magical' : 'hide'}>
               <Post posts={posts} />
-            </>;
+            </div>;
           }
           return null;
         })}
     </section>
     <section className='category-section'>
-    <h3 className='category'>mystery</h3>
+    <h3 className='category' onClick={ toggleClass }>mystery</h3>
         {data.map(posts => {
           const { tags } = posts;
           if (tags.includes('mystery')) {
-            return <>
+            return <div className={section ? 'show-mystery' : 'hide'}>
               <Post posts={posts} />
-            </>;
+            </div>;
           }
           return null;
         })}
     </section>
  </main>
-);
+  );
+};
 
 export default Blog;
